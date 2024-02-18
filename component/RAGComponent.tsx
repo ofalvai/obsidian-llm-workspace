29507f2233f1d2dd26fcd4859aabced952526ddc
Olivér Falvai <ofalvai@gmail.com>
2024-02-18 18:36:30 +0100

    Split components

--- component/RAGComponent.tsx
+++ component/RAGComponent.tsx
@@ -9,7 +9,9 @@ import { VectorStoreIndex } from "rag/storage"
 import { DumbResponseSynthesizer, QueryResponse } from "rag/synthesizer"
 import { LlmDexie, VectorStoreEntry } from "storage/db"
 import { AppContext, PluginSettingsContext, isLlmWorkspace } from "utils/obsidian"
-import { ChevronDown, ChevronUp, FileCheck2, FileX2, Folder } from "lucide-preact"
+import { EmbeddedFileInfo, NoteLinks } from "./NoteLinks"
+import { NonWorkspaceView, WorkspaceDetails } from "./WorkspaceDetails"
+import { QuestionAndAnswer } from "./QuestionAndAnswer"
 
 export const WorkspaceRAG = (props: { db: LlmDexie; file: TFile }) => {
 	const app = useContext(AppContext)
@@ -82,12 +84,12 @@ export const WorkspaceRAG = (props: { db: LlmDexie; file: TFile }) => {
 	}
 
 	const onLinkClick = (link: EmbeddedFileInfo) => {
-		app.workspace.openLinkText(link.path, "", 'tab')
-	 }
+		app.workspace.openLinkText(link.path, "", "tab")
+	}
 
 	const onLinkRebuild = (link: EmbeddedFileInfo) => {
 		// TODO
-	 }
+	}
 
 	const links = useLiveQuery(
 		async () => {
@@ -169,182 +171,6 @@ export const WorkspaceRAG = (props: { db: LlmDexie; file: TFile }) => {
 					onQuestionSubmit={onQuestionSubmit}
 					queryResponse={queryResponse}
 				/>
-			)}
-		</div>
-	)
-}
-
-interface EmbeddedFileInfo {
-	name: string
-	parent: TFolder | null
-	path: string
-	nodeCount: number
-	lastProcessed: number | undefined
-}
-
-const NonWorkspaceView = (props: { file: TFile }) => {
-	return (
-		<div>
-			<h4>Not a workspace yet</h4>
-			<p>
-				{props.file.basename} is not an LLM workspace yet. Update its frontmatter and try
-				again.
-			</p>
-		</div>
-	)
-}
-
-type WorkspaceDetailsProps = {
-	file: TFile
-	links: EmbeddedFileInfo[]
-	onRebuildAll: () => void
-	onLinkClick: (link: EmbeddedFileInfo) => void
-	onLinkRebuild: (link: EmbeddedFileInfo) => void
-}
-const WorkspaceDetails = ({
-	file,
-	links,
-	onRebuildAll,
-	onLinkClick,
-	onLinkRebuild,
-}: WorkspaceDetailsProps) => {
-	return (
-		<div>
-			<NoteLinks
-				file={file}
-				links={links}
-				onLinkClick={onLinkClick}
-				onLinkRebuild={onLinkRebuild}
-			/>
-			<button onClick={onRebuildAll}>Rebuild embedding DB</button>
-		</div>
-	)
-}
-
-type NoteLinksProps = {
-	file: TFile
-	links: EmbeddedFileInfo[]
-	onLinkClick: (link: EmbeddedFileInfo) => void
-	onLinkRebuild: (link: EmbeddedFileInfo) => void
-}
-const NoteLinks = ({ file, links, onLinkClick, onLinkRebuild }: NoteLinksProps) => {
-	return (
-		<div>
-			<h6>Linked notes</h6>
-			<ul>
-				{links.map((link) => (
-					<NoteLink link={link} onLinkClick={onLinkClick} onRebuild={onLinkRebuild} />
-				))}
-			</ul>
-			{links.length === 0 && <p>No links</p>}
-		</div>
-	)
-}
-
-type NoteLinkProps = {
-	link: EmbeddedFileInfo
-	onLinkClick: (link: EmbeddedFileInfo) => void
-	onRebuild: (link: EmbeddedFileInfo) => void
-}
-const NoteLink = ({ link, onLinkClick, onRebuild }: NoteLinkProps) => {
-	let label = ""
-	switch (link.nodeCount) {
-		case 0:
-			label = "Not indexed yet"
-			break
-		case 1:
-			label = "Indexed as 1 node "
-			// TODO: it should periodically re-render to update the timestamp
-			label += window.moment(link.lastProcessed).fromNow()
-			break
-		default:
-			label = `Indexed as ${link.nodeCount} nodes `
-			label += window.moment(link.lastProcessed).fromNow()
-	}
-	const [isCollapsed, setCollapsed] = useState(true)
-	return (
-		<div key={link.path} class="llm-workspace-link-container">
-			<div class="llm-workspace-link-header">
-				<span
-					class="llm-workspace-link-status-icon"
-					aria-label={label}
-					data-tooltip-position="top"
-					data-tooltip-delay="300"
-				>
-					{link.nodeCount == 0 && (
-						<FileX2 size={18} color="var(--background-modifier-error)" />
-					)}
-					{link.nodeCount > 0 && (
-						<FileCheck2 size={18} color="var(--background-modifier-success)" />
-					)}
-				</span>
-				<a class="llm-workspace-link-name" onClick={() => onLinkClick(link)}>{link.name}</a>
-				<div
-					class="clickable-icon llm-workspace-link-expand"
-					onClick={() => setCollapsed(!isCollapsed)}
-				>
-					{isCollapsed ? <ChevronDown size={18} /> : <ChevronUp size={18} />}
-				</div>
-			</div>
-
-			<div
-				className={"llm-workspace-link-details " + (isCollapsed ? "collapsed" : "expanded")}
-			>
-				{link.parent && !link.parent.isRoot() && (
-					<div class="llm-workspace-link-parent">
-						<Folder size={18} class="llm-workspace-link-icon-folder" />
-						{link.parent.name}
-					</div>
-				)}
-				<button onClick={() => onRebuild(link)}>Re-index file</button>
-			</div>
-		</div>
-	)
-}
-
-interface QuestionAndAnswerProps {
-	file: TFile
-	isLoading: boolean
-	queryResponse?: QueryResponse
-	onQuestionSubmit: (q: string) => void
-}
-
-const QuestionAndAnswer = (props: QuestionAndAnswerProps) => {
-	const [question, setQuestion] = useState("")
-	const onSubmit = (e: SubmitEvent) => {
-		if (question) {
-			props.onQuestionSubmit(question)
-		}
-		e.preventDefault()
-	}
-	const onInput = (e: InputEvent) => {
-		setQuestion((e.target as HTMLInputElement).value)
-	}
-
-	return (
-		<div>
-			<form onSubmit={onSubmit}>
-				<input
-					type="text"
-					disabled={props.isLoading}
-					placeholder="Ask a question"
-					value={question}
-					onInput={onInput}
-				/>
-				<button type="submit" disabled={props.isLoading}>
-					Send
-					{props.isLoading && "..."}
-				</button>
-			</form>
-			{props.queryResponse && (
-				<div>
-					<div class="llm-workspace-completion">{props.queryResponse.text}</div>
-					<ol>
-						{props.queryResponse.sources.map((node) => (
-							<li>{node.parent}</li>
-						))}
-					</ol>
-				</div>
 			)}
 		</div>
 	)
