2c0b8bd7a7fadd4501a0f042851dd3896e99b19b
Olivér Falvai <ofalvai@gmail.com>
2024-11-17 15:25:49 +0100

    claude-3-5-sonnet-20241022

--- src/config/settings.ts
+++ src/config/settings.ts
@@ -39,7 +39,7 @@ const MODELS = [
 	"gpt-4o-mini-2024-07-18",
 	"claude-3-haiku-20240307",
 	"claude-3-sonnet-20240229",
-	"claude-3-5-sonnet-20240620",
+	"claude-3-5-sonnet-20241022",
 	"claude-3-opus-20240229",
 ]
 
